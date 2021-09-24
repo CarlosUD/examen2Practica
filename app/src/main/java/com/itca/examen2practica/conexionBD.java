5286fae8aa596a3dde5b8c758d923046fbac9e1c
Carlos Eduardo Del cid Gaitán <82596224+CarlosUD@users.noreply.github.com>
2021-09-24 09:25:17 -0600

    Update conexionBD.java

--- app/src/main/java/com/itca/examen2practica/conexionBD.java
+++ app/src/main/java/com/itca/examen2practica/conexionBD.java
@@ -3,20 +3,20 @@ package com.itca.examen2practica;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class conexionBD extends SQLiteOpenHelper {
 
-    public conexionBD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public conexionBD(@Nullable Context context) {
         super(context, name, factory, version);
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table tb_notas(id INTEGER PRIMARY,Titulo text,Descripcion text,Autor text)");
+          sqLiteDatabase.execSQL("create table tb_bloc(id integer not null primary key autoincrement, Titulo text, Descripcion text, Autor text)");
     }
 
     @Override
